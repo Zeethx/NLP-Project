c5fe5af683c79db47e8ff65b2960bbfd9e0487c3
Raghav <36124189+Zeethx@users.noreply.github.com>
2021-10-16 16:39:22 -0400

    Update Code.py
    Implemented walmart search feature

--- Code.py
+++ Code.py
@@ -11,7 +11,7 @@ window.configure()
 #window.mainloop()
 
 def search(data):
-    speak(f"searching for {data} on walmart")
+    print(f"searching for {data} on walmart")
     wb.open("https://www.walmart.ca/search?q="+data)
 
 def grocery_list():
@@ -24,51 +24,55 @@ def grocery_list():
         else:
             exit_text += e
     print("Say any of the following to exit( " + exit_text + " )")
+    print("Grocery List Created!\n")
+    grocerylist = []
     while True:
         
         try:
             with speech_recognition.Microphone() as mic:
-                recognizer.adjust_for_ambient_noise(mic, duration=0.1)
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
 
                 audio = recognizer.listen(mic)
 
                 text = recognizer.recognize_google(audio)
                 text = text.lower()
-                for _ in range(50): time.sleep(0.1)
+
                 #recorded msg forwarding
+                print(f"Recorded:{text}")
                 if text in exit_phrases:
                     break
-                if "create" in text:
-                    grocery_list = []
-                    print("Grocery List Created!\n")
-                try:
-                    if("add" or "include") in text:
-                        textbreak = text.split(" ")
-                        print(text)
-                        if (textbreak[textbreak.index("add")+1] in (["one", "two", "three", "to"]) or range(100)):                            
-                            usevalue = textbreak.index("add") + 2
-                        else:
-                            usevalue = textbreak.index("add") + 1
-                        grocery_list.append(textbreak[usevalue])
-                        print("Added: "+ textbreak[usevalue])
-                        if ("search" in text) and :
-                            search(textbreak[usevalue])
+                
+                if("add" or "include") in text:
+                    textbreak = text.split(" ")
+                    usevalue = textbreak[textbreak.index("add")+1]
+                    if (usevalue in (["one", "two", "three", "to"]) or range(100)) and len(textbreak)>2:                            
+                        usevalue = textbreak.index("add") + 2
+                        grocerylist.append(textbreak[usevalue])
+                        print(f"Added: {textbreak[usevalue]}")
+                    else:
+                        usevalue = textbreak.index("add") + 1
+                        grocerylist.append(textbreak[usevalue])
+                        print(f"Added: {textbreak[usevalue]}")
 
-                except:
-                    print("Create a grocery list first!")
+                #remove item from list
+
                 if("show") in text:
-                    showlist()
+                    print(f"Grocery List:{showlist(grocerylist)}")
+
+                if ("search") in text:
+                    textbreak = text.split(" ")
+                    usevalue = textbreak[textbreak.index("search") + 1]
+                    if usevalue in grocerylist:
+                        search(usevalue)
+                    else:
+                        print("Try saying 'search [item] on walmart'(item should in your grocery list)")
         except speech_recognition.UnknownValueError:
             recognizer = speech_recognition.Recognizer()
             continue
 
 def showlist(grocery_list):
-    fstr = ""
-    for e in grocery_list:
-        fstr+=e + " "
-    return fstr
+
+    return grocery_list
     
 
 grocery_list()
-
-
